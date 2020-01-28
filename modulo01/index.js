3d8533758a77e3146d4824b8117570984832c820
Daniel Moreira <dmcardoso.99@hotmail.com>
2020-01-27 22:04:39 -0300

    Add CRUD routes

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,16 +2,38 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
 const users = ["Diego", "Cláudio", "Victor"];
 
-server.get("/teste", (request, response) => {
-    const nome = request.query.nome;
-    return response.json({ message: `Hello ${nome}` });
+server.get("/users", (request, response) => {
+    return response.json(users);
 });
 
 server.get("/users/:id", (request, response) => {
     const { id } = request.params;
     return response.json(users[id]);
+});
+
+server.post("/users", (request, response) => {
+    const { name } = request.body;
+    users.push(name);
+    return response.json(users);
+});
+
+server.put("/users/:id", (request, response) => {
+    const { id } = request.params;
+    const { name } = request.body;
+    users[id] = name;
+
+    return response.json(users);
+});
+
+server.delete("/users/:id", (request, response) => {
+    const { id } = request.params;
+    users.splice(id, 1);
+
+    return response.send();
 });
 
 server.listen(3333);
