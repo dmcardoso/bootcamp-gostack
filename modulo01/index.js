bb9a0ee8bd41e3f7349890928e2442684675dfd5
Daniel Moreira <dmcardoso.99@hotmail.com>
2020-01-27 21:42:46 -0300

    Add nodemon nd change users route

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,8 +2,16 @@ const express = require("express");
 
 const server = express();
 
+const users = ["Diego", "Cláudio", "Victor"];
+
 server.get("/teste", (request, response) => {
-    return response.json({ message: "Hello world" });
+    const nome = request.query.nome;
+    return response.json({ message: `Hello ${nome}` });
+});
+
+server.get("/users/:id", (request, response) => {
+    const { id } = request.params;
+    return response.json(users[id]);
 });
 
 server.listen(3333);
