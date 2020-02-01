de047cc779a6277d545168ab30df48148a0d54aa
Daniel Moreira <dmcardoso.99@hotmail.com>
2020-02-01 08:19:34 -0300

    Add middlewares lesson

--- modulo01/index.js
+++ modulo01/index.js
@@ -6,30 +6,64 @@ server.use(express.json());
 
 const users = ["Diego", "Cláudio", "Victor"];
 
+server.use((request, response, next) => {
+    console.time("Request");
+
+    console.log(`Método ${request.method}; URL: ${request.url};`);
+
+    next();
+
+    console.timeEnd("Request");
+});
+
+function checkUserExists(request, response, next) {
+    if (!request.body.name) {
+        return response.status(400).json({ error: "User name is required" });
+    }
+
+    return next();
+}
+
+function checkUserInArray(request, response, next) {
+    const user = users[request.params.id];
+
+    if (!user) {
+        return response.status(400).json({ error: "User does not exists" });
+    }
+
+    request.user = user;
+
+    return next();
+}
+
 server.get("/users", (request, response) => {
     return response.json(users);
 });
 
-server.get("/users/:id", (request, response) => {
-    const { id } = request.params;
-    return response.json(users[id]);
+server.get("/users/:id", checkUserInArray, (request, response) => {
+    return response.json(request.user);
 });
 
-server.post("/users", (request, response) => {
+server.post("/users", checkUserExists, (request, response) => {
     const { name } = request.body;
     users.push(name);
     return response.json(users);
 });
 
-server.put("/users/:id", (request, response) => {
-    const { id } = request.params;
-    const { name } = request.body;
-    users[id] = name;
+server.put(
+    "/users/:id",
+    checkUserInArray,
+    checkUserExists,
+    (request, response) => {
+        const { id } = request.params;
+        const { name } = request.body;
+        users[id] = name;
 
-    return response.json(users);
-});
+        return response.json(users);
+    }
+);
 
-server.delete("/users/:id", (request, response) => {
+server.delete("/users/:id", checkUserInArray, (request, response) => {
     const { id } = request.params;
     users.splice(id, 1);
 
