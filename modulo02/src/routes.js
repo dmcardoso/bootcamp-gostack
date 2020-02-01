99ac08042a4aca883046a06c15a1b248f3f9fe3a
Daniel Moreira <dmcardoso.99@hotmail.com>
2020-02-01 14:24:21 -0300

    :rocket: Add first lesson modulo02

---
+++ modulo02/src/routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const routes = new Router();
+
+routes.get("/", (request, response, next) => {
+  return response.json({
+    message: "Hello world"
+  });
+});
+
+export default routes;
