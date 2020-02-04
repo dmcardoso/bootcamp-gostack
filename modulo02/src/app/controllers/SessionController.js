1df2f4f2e423443a55efc14e1f5f6c735beae58d
Daniel Moreira <dmcardoso.99@hotmail.com>
2020-02-04 20:32:54 -0300

    :rocket: Finish modulo02

--- modulo02/src/app/controllers/SessionController.js
+++ modulo02/src/app/controllers/SessionController.js
@@ -1,10 +1,20 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import auth from '../../config/auth';
 import User from '../models/User';
 
 class SessionController {
     async store(request, response) {
+        const schema = Yup.object().shape({
+            email: Yup.string().required(),
+            password: Yup.string().required(),
+        });
+
+        if (!(await schema.isValid(request.body))) {
+            return response.status(400).json({ error: 'Validation fails' });
+        }
+
         const { email, password } = request.body;
 
         const user = await User.findOne({ where: { email } });
