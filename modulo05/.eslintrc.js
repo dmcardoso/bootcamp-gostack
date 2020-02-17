74f02b1e138c06799123028901d9e9cfc7e366e8
Daniel Moreira <dmcardoso.99@hotmail.com>
2020-02-17 10:58:45 -0300

    :rocket: Add code styles and config procjet infra

---
+++ modulo05/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends: ['plugin:react/recommended', 'airbnb', 'prettier/react'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['react', 'prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+        'react/jsx-filename-extension': [
+            'warn',
+            {
+                extensions: ['.jsx', '.js'],
+            },
+        ],
+        'import/prefer-default-export': 'off',
+        indent: ['error', 4, { SwitchCase: 1 }],
+        'no-param-reassign': 'off',
+        'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    },
+};
