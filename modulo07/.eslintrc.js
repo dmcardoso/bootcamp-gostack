7eae8d49ca8dd3b34c09d51a901871399764ddd5
Daniel Moreira <dmcardoso.99@hotmail.com>
2020-02-25 10:33:57 -0300

    :rocket: Add module07 files

---
+++ modulo07/.eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends: ['plugin:react/recommended', 'airbnb', 'prettier/react'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['react', 'prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+        'curly': ['error', 'all'],
+        'react/jsx-filename-extension': [
+            'warn',
+            {
+                'extensions': ['.jsx', '.js'],
+            },
+        ],
+        'quotes': [
+            'error',
+            'single',
+            { 'avoidEscape': true, 'allowTemplateLiterals': false }
+        ],
+        'max-len': ['error', {'code': 80, 'ignoreUrls': true}],
+        'import/prefer-default-export': 'off',
+        'indent': ['error', 4, { 'SwitchCase': 1 }],
+        'no-param-reassign': 'off',
+        'no-unused-vars': ['error', { 'argsIgnorePattern': 'next' }],
+        'arrow-parens': 'off',
+        'comma-dangle': [
+            'error',
+            {
+                'arrays': 'only-multiline',
+                'objects': 'only-multiline',
+                'imports': 'only-multiline',
+                'exports': 'only-multiline',
+                'functions': 'never',
+            },
+        ],
+        'no-console': ['error', {allow: ['tron']}],
+        'operator-linebreak': ['error', 'after', { "overrides": { "?": "before", ":": "before" } }],
+    },
+};
